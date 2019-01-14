e167811ebe52ffdd908dd7cb3613eb401b7b6dc7
weedjy <weedjy@gmail.com>
2019-01-14 15:58:56 +0300

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -6,11 +6,17 @@ from pytube import YouTube
 
 SAVE_DIR = os.path.expanduser('~')
 
-a = YouTube('https://www.youtube.com/watch?v=FtyYYYZ9Ga4')
+a = YouTube('https://www.youtube.com/watch?v=-----------')
 print(a.streams.all())
 stream = a.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 stream.download()
 os.rename(stream.default_filename, SAVE_DIR + '/' + a.title + '.mp4')
 
 
-# yt.streams.filter(only_audio=True).all()
+#a = YouTube('https://www.youtube.com/watch?v=------------')
+#for q in a.streams.filter(only_audio=True).all():
+#    print(q)
+
+#stream = a.streams.get_by_itag('171')
+#stream.download()
+#os.rename(stream.default_filename, SAVE_DIR + '/' + a.title + '.ogg')
