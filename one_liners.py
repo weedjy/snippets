66f9dccb8b135046d1071f3062bf5653c407087a
weedjy <weedjy@gmail.com>
2019-01-20 00:12:33 +0300

    Update one_liners.py

--- one_liners.py
+++ one_liners.py
@@ -1,3 +1,5 @@
 # given (hh:mm:ss)_1 and (hh:mm:ss)_2. find the difference in seconds
 print(sum([(60**((5-i)%3))*int(input())*int((i//3-0.5)*2.0) for i in range(6)]))
 
+import seaborn as sns
+ax = sns.regplot(x='input', y='output', data=df, color='green', marker='+')
